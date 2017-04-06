48e0218308d1b68606eb81986ad9dd2d3c0e0baa
ymiyauchi <ciero639@gmail.com>
2017-04-07 07:22:35 +0900

    add OnUIAcceptListener

---
+++ lib-commni/src/main/java/jp/gr/java_conf/falius/andcommuni/OnUIAcceptListener.java
@@ -0,0 +1,31 @@
+package jp.gr.java_conf.falius.andcommuni;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import jp.gr.java_conf.falius.communication.core.Server;
+
+/**
+ * Created by ymiyauchi on 2017/04/07.
+ */
+
+public abstract class OnUIAcceptListener implements Server.OnAcceptListener {
+    private final Handler mHandler;
+
+    public OnUIAcceptListener(Looper looper) {
+        mHandler = new Handler(looper);
+    }
+
+    @Override
+    public final void onAccept(final String remoteAddress) {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                onUIAccept(remoteAddress);
+            }
+        };
+        mHandler.post(r);
+    }
+
+    protected abstract void onUIAccept(String remoteAddress);
+}
